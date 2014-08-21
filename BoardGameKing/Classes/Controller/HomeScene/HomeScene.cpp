62b7504b02bfa3da27101a9b2b8f82821fed4566
Lai Lanfeng <lanfeng.lai@gumichina.com>
2014-08-21 11:57:49 +0800

    Add cocosbuilder

---
+++ BoardGameKing/Classes/Controller/HomeScene/HomeScene.cpp
@@ -0,0 +1,9 @@
+//
+//  HomeScene.cpp
+//  BoardGameKing
+//
+//  Created by 赖 览峰 on 14-8-20.
+//
+//
+
+#include "HomeScene.h"
---
+++ BoardGameKing/Classes/Controller/HomeScene/HomeScene.h
@@ -0,0 +1,14 @@
+//
+//  HomeScene.h
+//  BoardGameKing
+//
+//  Created by 赖 览峰 on 14-8-20.
+//
+//
+
+#ifndef __BoardGameKing__HomeScene__
+#define __BoardGameKing__HomeScene__
+
+#include <iostream>
+
+#endif /* defined(__BoardGameKing__HomeScene__) */
